8acb3f805d0d2b8280d74538e2fefa4f44d577c5
Nate Wessels <natewessels@Nates-iMac.local>
2019-01-25 09:52:23 -0800

    add conditional statements that check what number they are equal to

--- js/scripts.js
+++ js/scripts.js
@@ -17,42 +17,43 @@ function neither(input) {
   text = text + input + ", ";
 }
 
+
+
 function spliting(number) {
-  alert(number);
   debugger;
-  newNumber = number.split(" ");
-  var numberLength = newNumber.length;
+  var string = number.toString();
+  var newNumber = string.split("");
+
+  var numberLength = number.toString().length;;
   var haveOne = 0;
   var haveTwo = 0;
   var haveThree = 0;
+
   for (var a = 0; a < numberLength; a++) {
-    if (newNumber == 1) {
-      haveOne = 1;
-      haveTwo = 0;
-      haveThree = 0;
+    if (newNumber[a] == 1) {
+      haveOne = "1";
+      haveTwo = "0";
+      haveThree = "0";
     }
-    if (newNumber == 2) {
-      haveOne = 0;
-      haveTwo = 1;
-      haveThree = 0;
+    if (newNumber[a] == 2) {
+      haveOne = "0";
+      haveTwo = "1";
+      haveThree = "0";
     }
-    if (newNumber == 3) {
-      haveOne = 0;
-      haveTwo = 0;
-      haveThree = 1;
+    if (newNumber[a] == 3) {
+      haveOne = "0";
+      haveTwo = "0";
+      haveThree = "1";
     }
   }
   if (haveOne == 1) {
     isOne();
-  }
-  if (haveTwo == 2) {
+  }else if (haveTwo == 1) {
     isTwo();
-  }
-  if(haveThree == 2) {
+  }else if(haveThree == 1) {
     isThree();
-  }
-  if(haveOne != 1 || haveTwo != 1 || haveThree != 1) {
-    neither()
+  }else if(haveOne != 1 && haveTwo != 1 && haveThree != 1) {
+    neither(number);
   }
 
 
@@ -68,7 +69,6 @@ function spliting(number) {
 
 $(document).ready(function() {
   $("form#form").submit(function(event) {
-    debugger;
     var input = parseInt($("input#inputNumber").val());
     var numberArray = [];
 
@@ -76,9 +76,8 @@ $(document).ready(function() {
       numberArray[a] = a;
     }
     var arrayLength = numberArray.length;
-    for(var a = 0; a <= arrayLength; a++) {
-      var b = numberArray[a];
-      alert(numberArray);
+    for(var a = 0; a < arrayLength; a++) {
+      var b = numberArray[a].toString();
       spliting(b);
     }
     alert(text);
